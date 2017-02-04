ed750c6562122bfe18a742d4486683f272c2cbc0
David Power <david.power@wpengine.com>
2017-02-04 22:14:04 +0000

    add basic navigation and UI

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @articles_path = '/articles/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  root 'articles#new'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
